491807238f1a66f1caf95b5a154e613523abcabd
Karen Cardoz <karen.cardoz@rangle.io>
2023-06-28 13:31:26 -0400

    initial svg for media module added

--- src/demos/media-module.js
+++ src/demos/media-module.js
@@ -1,10 +1,33 @@
-import React from "react";
-import "./media-module.css";
+import React from 'react'
+import './media-module.css'
 
 const MediaModule = () => {
   return (
-    <h1> media-module </h1>
-  );
-};
+    <div>
+      <div className="multiPathContainer">
+        <h2>Media Module 2.0</h2>
+        <svg
+          width="316"
+          height="229"
+          viewBox="0 0 316 229"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <image href="people-working.png" height="229" width="316" />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M228.858 0V228.789H0L228.858 0Z"
+            fill="#CF4326"
+          />
+          <path
+            d="M184.963 13.459H18.8203V215.331H316.001L184.963 13.459Z"
+            fill="#F7856E"
+          />
+        </svg>
+      </div>
+    </div>
+  )
+}
 
-export default MediaModule;
+export default MediaModule
